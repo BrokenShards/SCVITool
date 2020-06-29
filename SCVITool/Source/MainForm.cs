b3ed34c91f820a768136c18d2282f0dc96d16f67
Shard White <breakingshards@gmail.com>
2020-06-29 17:26:45 +0100

    Refactor
    The project has been refactored, seperating backup and restore logic.

---
+++ SCVITool/Source/MainForm.cs
@@ -0,0 +1,27 @@
+﻿// MainForm.cs //
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SCVITool
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		private void BackupClicked( object sender, EventArgs e )
+		{
+			if( !SaveManager.Backup() )
+				MessageBox.Show( this, SaveManager.ErrorMessage, "Backup failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+		}
+		private void RestoreClicked( object sender, EventArgs e )
+		{
+			if( !SaveManager.Restore() )
+				MessageBox.Show( this, SaveManager.ErrorMessage, "Restore failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+		}
+	}
+}
